831321b765144cb5d8bbf2673c91af4a6c43f111
Koen Groot <kwgroot@gmail.com>
2020-12-28 17:54:55 +0100

    Realized mistake made with Dao and database, fixed.

--- app/src/main/java/com/example/madlevel5task2/UI/BacklogFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/UI/BacklogFragment.kt
@@ -6,13 +6,88 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel5task2.R
+import com.example.madlevel5task2.Viewmodel.Game
+import com.example.madlevel5task2.Viewmodel.GameViewModelViewModel
+import kotlinx.android.synthetic.main.fragment_backlog_game.*
+import androidx.lifecycle.Observer
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class BacklogFragment : Fragment() {
+class GameBacklogFragment : Fragment() {
 
+    private lateinit var gameAdapter: GameAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
+    private var games: ArrayList<Game> = arrayListOf()
+    private val viewModel: GameViewModelViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_backlog_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRv()
+        observeAddGameResult()
+    }
+
+    private fun observeAddGameResult() {
+        viewModel.games.observe(viewLifecycleOwner, Observer { games ->
+            this@GameBacklogFragment.games.clear()
+            this@GameBacklogFragment.games.addAll(games)
+
+            /**
+             * Sorting by date
+             */
+            this@GameBacklogFragment.games.sortBy {
+                it.releaseDate
+            }
+            gameAdapter.notifyDataSetChanged()
+
+        })
+    }
+
+    private fun initRv() {
+
+        gameAdapter = GameAdapter(games)
+        viewManager = LinearLayoutManager(activity)
+
+        createItemTouchHelper().attachToRecyclerView(rvGameBacklog)
+
+        rvGameBacklog.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = gameAdapter
+        }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val gameToDelete = games[position]
+                viewModel.deleteGame(gameToDelete)
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
 }
